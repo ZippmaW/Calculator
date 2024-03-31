dbb5a84ec72a68d895efd91e8d2ebdf35c362f80
Kevin Ukota <z1ppm4n@gmail.com>
2024-03-31 15:50:23 +0200

    Create three variables for each of the parts of a calculation and create a new function operate() that takes and operator, two numbers and returns the the value from the correct operation

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 "use strict";
 
+let firstNumber;
+let secondNumber;
+let mainOperator;
+
 function add(numA, numB) {
   return numA + numB;
 }
@@ -14,4 +18,17 @@ function multiply(numA, numB) {
 
 function divide(numA, numB) {
   return numA / numB;
+}
+
+function operate(num1, num2, operator) {
+  switch (operator) {
+    case "add":
+      return add(num1, num2);
+    case "subtract":
+      return subtract(num1, num2);
+    case "multiply":
+      return multiply(num1, num2);
+    case "divide":
+      return divide(num1, num2);
+  }
 }
