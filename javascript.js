df351592ff1e109063cf9b9a77ee413c757c2b76
Kevin Ukota <z1ppm4n@gmail.com>
2024-03-31 15:26:10 +0200

    Create basic template of the HTML file, link js and css files to it and create the basic math functions add, subtract, multiply and divide in the js script

---
+++ javascript.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function add(numA, numB) {
+  return numA + numB;
+}
+
+function subtract(numA, numB) {
+  return numA - numB;
+}
+
+function multiply(numA, numB) {
+  return numA * numB;
+}
+
+function divide(numA, numB) {
+  return numA / numB;
+}
