3b24cc4f1bfa902d62db5822c369b24bac150807
Kevin Ukota <z1ppm4n@gmail.com>
2024-04-08 15:57:58 +0200

    Create functions and event listeners for basic operations without decimals or equations of multiple numbers

--- javascript.js
+++ javascript.js
@@ -32,3 +32,44 @@ function operate(num1, num2, operator) {
       return divide(Number(num1), Number(num2));
   }
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const display = document.querySelector(".display");
+  document.querySelectorAll(".numBTN").forEach((element) =>
+    element.addEventListener("click", function (e) {
+      if (!mainOperator) {
+        if (!firstNumber) {
+          firstNumber = e.target.textContent;
+          display.innerHTML = e.target.textContent;
+        } else {
+          firstNumber += e.target.textContent;
+          display.innerHTML = firstNumber;
+        }
+      } else {
+        if (!secondNumber) {
+          secondNumber = e.target.textContent;
+          display.innerHTML = e.target.textContent;
+        } else {
+          secondNumber += e.target.textContent;
+          display.innerHTML = secondNumber;
+        }
+      }
+    })
+  );
+  document.querySelectorAll(".operators").forEach((elem) =>
+    elem.addEventListener("click", function (e) {
+      if (!mainOperator) {
+        mainOperator = e.target.id;
+        console.log(mainOperator);
+      }
+    })
+  );
+  document.querySelector("#equals").addEventListener("click", function () {
+    display.innerHTML = operate(firstNumber, secondNumber, mainOperator);
+  });
+  document.querySelector("#clear").addEventListener("click", function () {
+    firstNumber = false;
+    secondNumber = false;
+    mainOperator = false;
+    display.innerHTML = "";
+  });
+});
