c8d15974e4f646d6af658503576838a857cacde3
Kevin Ukota <z1ppm4n@gmail.com>
2024-04-10 12:11:43 +0200

    Extra: Create/modify to include floats

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
 "use strict";
 
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
 let mainOperator;
+let lastAction;
+let float = false;
 
 function add(numA, numB) {
   return numA + numB;
@@ -39,34 +41,78 @@ document.addEventListener("DOMContentLoaded", function () {
   const display = document.querySelector(".display");
   document.querySelectorAll(".numBTN").forEach((element) =>
     element.addEventListener("click", function (e) {
+      if (float && lastAction === 1) {
+        firstNumber += e.target.textContent;
+        display.innerHTML = firstNumber;
+        console.log("float1");
+        return;
+      }
+      if (float && lastAction === 2) {
+        secondNumber += e.target.textContent;
+        display.innerHTML = secondNumber;
+        console.log("float2");
+        return;
+      }
       if (!mainOperator) {
         if (!firstNumber) {
           firstNumber = e.target.textContent;
           display.innerHTML = e.target.textContent;
+          lastAction = 1;
+          console.log("float3");
         } else {
           firstNumber += e.target.textContent;
           display.innerHTML = firstNumber;
+          lastAction = 1;
+          console.log("float4");
         }
       } else {
         if (!secondNumber) {
           secondNumber = e.target.textContent;
           display.innerHTML = e.target.textContent;
+          lastAction = 2;
+          console.log("float5");
         } else {
-          secondNumber = e.target.textContent;
+          secondNumber += e.target.textContent;
           display.innerHTML = secondNumber;
+          lastAction = 2;
+          console.log("float6");
         }
       }
     })
   );
   document.querySelectorAll(".operators").forEach((elem) =>
     elem.addEventListener("click", function (e) {
-      if (!mainOperator) {
+      if (
+        e.target.id === "float" &&
+        (!firstNumber.toString().includes(".") ||
+          !secondNumber.toString().includes("."))
+      ) {
+        if (lastAction === 1) {
+          firstNumber += ".";
+          display.innerHTML = firstNumber;
+          float = true;
+          console.log("oper1");
+          return;
+        }
+        if (lastAction === 2) {
+          secondNumber += ".";
+          display.innerHTML = secondNumber;
+          float = true;
+          console.log("oper2");
+          return;
+        }
+      }
+      if (e.target.id !== "float" && !mainOperator) {
         mainOperator = e.target.id;
-      } else {
+        float = false;
+        console.log("oper3");
+      } else if (e.target.id !== "float" && float) {
         firstNumber = operate(firstNumber, secondNumber, mainOperator);
         secondNumber = false;
+        float = false;
         display.innerHTML = firstNumber;
         mainOperator = e.target.id;
+        console.log("oper4");
       }
     })
   );
@@ -79,6 +125,7 @@ document.addEventListener("DOMContentLoaded", function () {
     firstNumber = false;
     secondNumber = false;
     mainOperator = false;
+    float = false;
     display.innerHTML = "";
   });
 });
